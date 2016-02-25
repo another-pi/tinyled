3aa4419234561d9049186d3c0e93f7e871a64f86
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-02-25 20:52:35 +0300

    add comments

--- main.c
+++ main.c
@@ -24,23 +24,23 @@ int main(void) {
     int8_t direction = 1;
     int8_t hold = -1;
     set_sleep_mode(SLEEP_MODE_IDLE);
-    PORTB = 0xff;
-    DDRB |= (1 << PB1);
-    TCCR0B |= (1<<CS01);
-    TCCR0A |= (1 << WGM01) | (1 << WGM00);
-    TCCR0A |= (1 << COM0B1) | (1 << COM0B0);
-    TIMSK0 |= (1 << TOIE0);
-    ADCSRA &= ~(1<<ADEN);
+    PORTB = 0xff;  // enable pullup resistors for powersaving
+    DDRB |= (1 << PB1);  // set pwm port as output
+    TCCR0B |= (1<<CS01);  // timer prescaler == 8
+    TCCR0A |= (1 << WGM01) | (1 << WGM00); // set pwm to fast_pwm mode
+    TCCR0A |= (1 << COM0B1) | (1 << COM0B0); // set pwm to inverting mode
+    TIMSK0 |= (1 << TOIE0); // enable timer interrupt
+    ADCSRA &= ~(1<<ADEN);  //disable ADC
     while (PINB & (1<<PB3)) {
-        ++s;
+        ++s;  // increment s until button is pressed
     }
     sei();
     while(1) {
-        if (occount >= cap) {
-            cli();
-            if (power == 0xff || power <= 0x70) {
+        if (occount >= cap) { // cap sets the delay for updating pwm value
+            cli(); // we don't need interrupts while updating pwm
+            if (power == 0xff || power <= 0x70) { // change fade direction after reaching max\min values
                 direction = (int8_t)(direction * -1);
-                hold = (int8_t)(hold * direction);
+                hold = (int8_t)(hold * direction); // skip every two dimming phases
                 cap = (uint8_t)(rnd() % 112);
             }
             power += direction;
