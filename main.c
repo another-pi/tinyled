cde8240c5e7f3306cbb98fb6d73436a1be8a647a
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-02-25 20:57:41 +0300

    rand func reformat, allow higher cap

--- main.c
+++ main.c
@@ -5,13 +5,13 @@
 #include <avr/sleep.h>
 
 register uint8_t occount asm("r2");
-register uint8_t s asm("r3");
-register uint8_t a asm("r4");
+register uint8_t s asm("r3"); // lcg var
+register uint8_t a asm("r4"); // lcg var
 
-static uint8_t rnd(void) {
-    s^=s<<3;
-    s^=s>>5;
-    s^=a++>>2;
+static uint8_t rand(void) {
+    s ^= s << 3;
+    s ^= s >> 5;
+    s ^= a++ >> 2;
     return s;
 }
 
@@ -25,6 +25,7 @@ int main(void) {
     int8_t direction = 1;
     int8_t hold = -1;
     occount = 0;
+
     set_sleep_mode(SLEEP_MODE_IDLE);
     PORTB = 0xff;  // enable pullup resistors for powersaving
     DDRB |= (1 << PB1);  // set pwm port as output
@@ -33,9 +34,11 @@ int main(void) {
     TCCR0A |= (1 << COM0B1) | (1 << COM0B0); // set pwm to inverting mode
     TIMSK0 |= (1 << TOIE0); // enable timer interrupt
     ADCSRA &= ~(1<<ADEN);  //disable ADC
+    
     while (PINB & (1<<PB3)) {
         ++s;  // increment s until button is pressed
     }
+    
     sei();
     while(1) {
         if (occount >= cap) { // cap sets the delay for updating pwm value
@@ -43,7 +46,7 @@ int main(void) {
             if (power == 0xff || power <= 0x70) { // change fade direction after reaching max\min values
                 direction = (int8_t)(direction * -1);
                 hold = (int8_t)(hold * direction); // skip every two dimming phases
-                cap = (uint8_t)(rnd() % 112);
+                cap = (uint8_t)(rand() % 200);
             }
             power += direction;
             if (hold == 1)
